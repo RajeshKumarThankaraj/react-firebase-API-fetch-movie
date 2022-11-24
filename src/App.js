7edc53221c1a4e0cb50a8de0cfe7869ac8e53ae4
Rajesh Kumar Thankaraj <thankaraja@gmail.com>
2022-11-24 23:23:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-1cfb5-default-rtdb.firebaseio.com/movies.json"
+        ""
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
